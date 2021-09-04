dbc39fcb89e2e68ca76ca37a451e87cc553cfbee
Ruben <rubenfb14@gmail.com>
2021-09-04 17:59:37 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { randomBagId, useBag } from "./hooks";
+
+function App() {
+  const [bagId, setBagId] = useState(randomBagId());
+  const [svgMode, setSvgMode] = useState(false);
+  const bag = useBag(bagId);
+  return (
+    <div className="app">
+      <div className="header">
+        <h1>Gear Rarity Demo</h1>
+        <h2>
+          <label htmlFor="bag-input">Bag #</label>
+
+          <input
+            id="bag-input"
+            type="text"
+            value={bagId ?? ""}
+            onChange={(event) => {
+              const value = event.currentTarget.value.trim();
+              if (value === "") {
+                setBagId("");
+                return;
+              }
+              const numId = Number(value);
+              if (!isNaN(numId) && numId > 0 && numId <= 8000) {
+                setBagId(value);
+              }
+            }}
+          />
+          <button onClick={() => setBagId(randomBagId())}>random</button>
+        </h2>
+        <div className="svg-mode">
+          <label>
+            svg mode
+            <input
+              type="checkbox"
+              checked={svgMode}
+              onChange={() => setSvgMode((v) => !v)}
+            />
+          </label>
+        </div>
+      </div>
+      {bag &&
+        (svgMode ? (
+          <div className="bag">
+            <img src={bag.image} alt="" />
+          </div>
+        ) : (
+          <ul className="bag">
+            {bag?.items.map(({ color = "#ffffff", name }, index) => {
+              return (
+                <li key={name + index} style={{ color }}>
+                  {name}
+                </li>
+              );
+            })}
+          </ul>
+        ))}
+      <footer>
+        <p>
+          {/*Custom colors: <span style={{ color: "cyan" }}>divine</span> items,{" "}
+          <span style={{ color: "crimson" }}>divine robes</span>.*/}
+        </p>
+        <p>
+          <a href="https://github.com/bpierre/gear-rarity" target="_blank">
+            GitHub
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks.tsx
@@ -0,0 +1,47 @@
+import {
+  ColorFnParameters,
+  rarityColor,
+  rarityDescription,
+  rarityImageFromItems
+} from "gear-rarity";
+import loot from "../data/loot.json";
+
+type ItemRarityInfo = {
+  color?: string;
+  name: string;
+  description?: string;
+};
+
+function colorFn({ itemName }: ColorFnParameters) {
+  // const name = itemName?.toLowerCase() ?? "";
+  // if (name.includes("divine robe")) return "crimson";
+  // if (name.includes("divine")) return "cyan";
+}
+
+function rarityInfo(name: string): ItemRarityInfo {
+  const color = rarityColor(name, { colorFn });
+  const description = rarityDescription(name);
+  return { color, description, name };
+}
+
+export function randomBagId() {
+  return String(Math.floor(Math.random() * 7999) + 1);
+}
+
+export function useBag(
+  id: string
+): null | { id: string; items: Array<ItemRarityInfo>; image: string } {
+  const bagId = Number(id);
+
+  if (isNaN(bagId) || bagId < 1 || bagId > 8000) {
+    return null;
+  }
+
+  const itemNames = Object.values(loot[bagId - 1][bagId]) as string[];
+
+  return {
+    id: String(bagId),
+    image: rarityImageFromItems(itemNames, { colorFn }),
+    items: itemNames.map(rarityInfo)
+  };
+}
